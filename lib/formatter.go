c3995a86bcb60431edc2bf51166fce50262004c5
Dheeraj Sharma <53325994+wayne9499@users.noreply.github.com>
2022-11-29 17:31:44 +0000

    add logger level

--- lib/formatter.go
+++ lib/formatter.go
@@ -11,6 +11,7 @@ type Formatter struct {
 	LogFormatter log.TextFormatter
 }
 
+// A custom formatter for the logger.
 func (f *Formatter) Format(entry *log.Entry) ([]byte, error) {
 	// example [2022-11-25 13:25:15] - [INFO] log-message...
 	return []byte(fmt.Sprintf("[%s] - [%s] %s\n", entry.Time.Format(f.LogFormatter.TimestampFormat), strings.ToUpper(entry.Level.String()), entry.Message)), nil
--- logger.go
+++ logger.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"os"
+
 	log "github.com/sirupsen/logrus"
 	"github.com/wayne9499/glogger/lib"
 )
@@ -9,14 +11,27 @@ type Logger struct {
 	Level log.Level
 }
 
+// Parsing the log level from the passing argument.
+func (l *Logger) parseLoggerLevel(lvl string) (log.Level, error) {
+	level, err := log.ParseLevel(lvl)
+	if err != nil {
+		return log.DebugLevel, err
+	}
+	return level, nil
+}
+
+// Creating a new logger and setting the log level and formatter.
 func (l *Logger) GetLogger() *log.Logger {
 	logger := log.New()
+	level := os.Getenv("LOG_LEVEL")
+	logLevel, _ := l.parseLoggerLevel(level)
+	logger.Level = logLevel
 	logger.Formatter = &lib.Formatter{
 		LogFormatter: log.TextFormatter{
 			FullTimestamp:          true,
 			TimestampFormat:        lib.REFERENCE_TIME,
-			ForceColors:            true,
-			DisableLevelTruncation: true,
+			ForceColors:            false,
+			DisableLevelTruncation: false,
 		},
 	}
 	return logger
